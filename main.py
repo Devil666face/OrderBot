71f5cf933b715bf960c3f8200e3c1ca8afb23429
root <root@vm3817913.25ssd.had.wf>
2023-02-14 14:24:01 +0000

    fmt

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 if __name__ == "__main__":
     scheduler = AsyncIOScheduler()
-    # scheduler.add_job(task_send_document, "interval", minutes=5)
-    scheduler.add_job(task_send_document, "interval", seconds=10)
+    scheduler.add_job(task_send_document, "interval", minutes=5)
+    # scheduler.add_job(task_send_document, "interval", seconds=10)
     scheduler.start()
     bot_poling()
