c2bcacedb05df9d416782c2b2827375039516aaa
root <root@kpsn33-gi-kk>
2023-02-14 16:35:08 +0300

    fmt

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ python3.10 -m venv venv
 ./venv/bin/pip install -r requirements.txt
 ./venv/bin/python manage.py migrate
 ./venv/bin/python manage.py loaddata dump.json
-ln -sf ./OrderBot.service /etc/systemd/system/OrderBot.service
+cp ./OrderBot.service /etc/systemd/system/OrderBot.service
 chown -R www-data:www-data ../OrderBot
 systemctl daemon-reload
 systemctl enable OrderBot
